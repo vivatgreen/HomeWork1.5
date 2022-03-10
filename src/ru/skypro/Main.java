9d649037448f705b680c59c762f44a3626156972
vivatgreen <grindsasha@bk.ru>
2022-03-10 21:36:50 +0300

    Commit 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
         // Задача 2
-        int clientDeviceYear = 2016;
+        int clientDeviceYear = 2014;
         int deviceOS = 1;
         if (clientDeviceYear < 2015 && deviceOS == 1) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
@@ -22,29 +22,30 @@ public class Main {
             } else if (clientDeviceYear >= 2015 && deviceOS == 0) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             }
-        }
-        // Задача 3
-        int year = 2020;
-        boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
-        if (isLeapYear) {
-            System.out.println(year + " високосный год");
-        } else {
-            System.out.println(year + " не високосный год");
-        }
-        // Задача 4
-        int deliveryDistance = 95;
-        int addedDeliveryTime = 0;
-        if (deliveryDistance <= 20) {
-            addedDeliveryTime += 1;
-            System.out.println("Потребуется дней:" + addedDeliveryTime);
-        } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-            addedDeliveryTime += 2;
-            System.out.println("Потребуется дней:" + addedDeliveryTime);
-        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-            addedDeliveryTime += 3;
-            System.out.println("Потребуется дней:" + addedDeliveryTime);;
-        } else
-            System.out.println("извините, доставка только в пределах МКАДа");
+            }
+            // Задача 3
+            int year = 2020;
+            boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+            if (isLeapYear) {
+                System.out.println(year + " високосный год");
+            } else {
+                System.out.println(year + " не високосный год");
+            }
+            // Задача 4
+            int deliveryDistance = 95;
+            int addedDeliveryTime = 0;
+            if (deliveryDistance <= 20) {
+                addedDeliveryTime += 1;
+                System.out.println("Потребуется дней:" + addedDeliveryTime);
+            } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+                addedDeliveryTime += 2;
+                System.out.println("Потребуется дней:" + addedDeliveryTime);
+            } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+                addedDeliveryTime += 3;
+                System.out.println("Потребуется дней:" + addedDeliveryTime);
+                ;
+            } else
+                System.out.println("извините, доставка только в пределах МКАДа");
 
             // Задача 5
             int monthNumber = 13;
@@ -74,5 +75,7 @@ public class Main {
             }
         }
     }
+
+
 
 
